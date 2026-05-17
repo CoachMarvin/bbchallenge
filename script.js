0fc605dfec597c935b132409348c4c8b6ab47e63
CoachMarvin <premium.coachmarvin@gmail.com>
2026-05-17 19:49:28 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //Firebase Module direkt aus dem Internet laden
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getFirestore, collection, addDoc, getDocs, query, onSnapshot, orderBy } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
-import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getAuth, signInWithEmailAndPassword, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
 // Exakte Firebase Konfiguration
 const firebaseConfig = {
